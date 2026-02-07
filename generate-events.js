526c36f531db09577520a3159f7210e37ea2a43d
srkdaniel1997-pixel <srkdaniel1997@gmail.com>
2026-02-07 12:00:31 +0000

    update

--- generate-events.js
+++ generate-events.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 const files = fs.readdirSync("./targets-src");
 
 const names = files
-  .filter(f=>f.endsWith(".jpg"))
-  .map(f=>f.replace(".jpg",""));
+  .filter(f => f.endsWith(".jpeg"))
+  .map(f => f.replace(".jpg",""));
 
-fs.writeFileSync("events.json", JSON.stringify(names, null, 2));
+fs.writeFileSync("events.json", JSON.stringify(names,null,2));
 
-console.log("events.json generated!");
+console.log("events.json updated!");
